ad2b9d43f3b6bc40731af5937ebb6367f938eb54
jeison fernandez <jeison.fernandez@sinco.com.co>
2025-11-02 20:23:55 -0500

    🔴 Se agrega el test Mostrar_Hacer_ejercicio_entre_las_0600_y_las_0659

---
+++ MorningRoutineTest.cs
@@ -0,0 +1,30 @@
+﻿using AwesomeAssertions;
+
+namespace MorningRoutine;
+
+public class MorningRoutineTest
+{
+    [Fact]
+    public void Mostrar_Hacer_ejercicio_entre_las_0600_y_las_0659()
+    {
+        //Arrange
+        var rutina = new Rutina("6:10");
+        //Act
+        
+        //Assert
+        rutina.WhatShouldIDoNow().Should().Be("Hacer ejercicio");
+    }
+}
+
+public class Rutina(string horaRutina) :IMorningRoutine
+{
+    public string WhatShouldIDoNow()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IMorningRoutine
+{
+    string WhatShouldIDoNow();
+}
---
+++ TestList.md
@@ -0,0 +1,4 @@
+## lista de test
+- [x] Escenario 1: Mostrar "Hacer ejercicio" entre las 06:00 y las 06:59 
+
+
